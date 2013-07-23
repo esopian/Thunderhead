8e1dda48d439d6252d4dc0a05ae3e6aba20b61dd
Evan Mallory <evan@cogsy.com>
2013-07-23 10:56:07 -0500

    fixing linter errors

--- demo/public/app.js
+++ demo/public/app.js
@@ -1,101 +1,102 @@
 require(['libs/text!header.html', 'libs/text!home.html', 'libs/text!footer.html', 'examples'], function (headerTpl, homeTpl, footerTpl, examples) {
-	
-	var ApplicationRouter = Backbone.Router.extend({
-		routes: {
-			"": "home",
-			"example/:name": "loadExample"
-		},
-		initialize: function() {
-			this.headerView = new HeaderView();
-			this.headerView.render();
-			this.footerView = new FooterView();
-			this.footerView.render();
-		},
-		home: function() {
-			this.homeView = new HomeView();
-			this.homeView.render();
-		},
-		loadExample : function(name) {
-			if(!this.homeView) { this.home(); }
-			this.homeView.loadExample(examples[name]);
-		}
-	});
 
-	HeaderView = Backbone.View.extend({
-		el: "#header",
-		templateFileName: "header.html",
-		template: headerTpl,
+    var ApplicationRouter = Backbone.Router.extend({
+        routes: {
+            "": "home",
+            "example/:name": "loadExample"
+        },
+        initialize: function() {
+            this.headerView = new HeaderView();
+            this.headerView.render();
+            this.footerView = new FooterView();
+            this.footerView.render();
+        },
+        home: function() {
+            this.homeView = new HomeView();
+            this.homeView.render();
+        },
+        loadExample : function(name) {
+            if(!this.homeView) { this.home(); }
+            this.homeView.loadExample(examples[name]);
+        }
+    });
 
-		initialize: function() {
-			
-		},
-		render: function() {
-			$(this.el).html(_.template(this.template));
-		}
-	});
+    HeaderView = Backbone.View.extend({
+        el: "#header",
+        templateFileName: "header.html",
+        template: headerTpl,
 
-	FooterView = Backbone.View.extend({
-		el: "#footer",
-		template: footerTpl,
-		render: function() {
-			this.$el.html(_.template(this.template));
-		}
-	});
+        initialize: function() {
+        },
+
+        render: function() {
+            $(this.el).html(_.template(this.template));
+        }
+    });
+
+    FooterView = Backbone.View.extend({
+        el: "#footer",
+        template: footerTpl,
+        render: function() {
+            this.$el.html(_.template(this.template));
+        }
+    });
 
 
-	HomeView = Backbone.View.extend({
-		el: "#content",
-		template: homeTpl,
-		events : {
-			'click .send' : 'sendRequest'
-		},
+    HomeView = Backbone.View.extend({
+        el: "#content",
+        template: homeTpl,
+        events : {
+            'click .send' : 'sendRequest'
+        },
 
-		initialize: function() {
+        initialize: function() {
 
-		},
-		render: function() {
-			$(this.el).html(_.template(this.template));
-		},
+        },
+        render: function() {
+            $(this.el).html(_.template(this.template));
+        },
 
-		sendRequest: function(e) {
-			var self = this;
+        sendRequest: function(e) {
+            var self = this;
 
-			self.$('.result').html("");
+            self.$('.result').html("");
 
-			var service = this.$('.service_select').val();
-			var command = this.$('.command_input').val();
-			var request = window.jsoneditor.get();
+            var service = this.$('.service_select').val();
+            var command = this.$('.command_input').val();
+            var request = window.jsoneditor.get();
 
-			$.ajax({
-			  type: 'POST',
-			  url: '/command/'+service+'/'+command,
-			  data: request,
-			  dataType: 'json',
-			  success: function(data, textStatus, jqXHR) {
-			  	self.$('.result').html(JSON.stringify(data, undefined, 2));
-			  },
-			  error : function(data) {
-			  	self.$('.result').html("!!!ERROR!!!\n\n"+data.responseText);
-			  }
-			});
-		},
+            $.ajax({
+                type: 'POST',
+                url: '/command/'+service+'/'+command,
+                data: request,
+                dataType: 'json',
+                success: function(data, textStatus, jqXHR) {
+                    self.$('.result').html(JSON.stringify(data, undefined, 2));
+                },
+                error : function(data) {
+                    self.$('.result').html("!!!ERROR!!!\n\n"+data.responseText);
+                }
+            });
+        },
 
-		loadExample: function(example) {
-			this.$('.service_select').val(example.service);
-			this.$('.command_input').val(example.command);
-			window.jsoneditor.set(example.request);
+        loadExample: function(example) {
+            this.$('.service_select').val(example.service);
+            this.$('.command_input').val(example.command);
+            window.jsoneditor.set(example.request);
 
             var self = this;
             //Load Example Text
             if(example.example) {
+                self.$('.code_example pre').html('');
                 self.$('.code_example').show();
                 $.ajax({
                     url : "/example_calls/"+example.example,
-                    dataType : 'text', 
+                    dataType : 'text',
                     cache : false,
 
                     success: function(data) {
-                        //Fix for <> 
+                        //Fix for <>
                         data = data.replace(/\</g, '&lt;');
                         data = data.replace(/\</g, '&gt;');
                         // console.log(data);
@@ -108,12 +109,11 @@ require(['libs/text!header.html', 'libs/text!home.html', 'libs/text!footer.html'
                 self.$('.code_example pre').html('');
                 self.$('.code_example').hide();
             }
-		}
-	});
-	
-	
-	app = new ApplicationRouter();
-	Backbone.history.start();	
+        }
+    });
+
+    app = new ApplicationRouter();
+    Backbone.history.start();
 });
 
 
